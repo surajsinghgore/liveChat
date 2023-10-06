ffa1386655619b13df018d98eabb31725580f376
surajsinghgore <surajthakurrs45@gmail.com>
2023-10-06 09:40:36 +0530

    user count style fix

--- public/script.js
+++ public/script.js
@@ -1,12 +1,13 @@
-const socket = io('/');
-var username = prompt("Enter Your Name before Joining Into Chat");
-var gender = prompt("Enter Your Gender before Joining Into Chat");
+const socket = io("/");
+// var username = prompt("Enter Your Name before Joining Into Chat");
+// var gender = prompt("Enter Your Gender before Joining Into Chat");
+let username = "suraj";
+let gender = "mlae";
 
-
-if((username==null)||(username=="")){
+if (username == null || username == "") {
   username = prompt("Enter Your Name before Joining Into Chat");
 }
-if((gender==null)||(gender=="")){
+if (gender == null || gender == "") {
   gender = prompt("Enter Your Gender before Joining Into Chat");
 }
 // send new username to serve
@@ -15,7 +16,7 @@ socket.emit("new-user-join", { username, gender });
 // self welcome message to current user
 socket.on("self-welcome", (username) => {
   var elements = document.getElementById("message_container");
-elements.innerHTML+=`<div class="welcome">
+  elements.innerHTML += `<div class="welcome">
 welcome to Live Chat <span>${username}</span>
 </div>`;
 });
@@ -29,31 +30,28 @@ socket.on("self-count", (count) => {
 });
 // notify every one that new user join the chat
 socket.on("notify-new-user-to-all", (username) => {
-  var audio = new Audio('newuser.mp3');
+  var audio = new Audio("newuser.mp3");
   audio.play();
   var elements = document.getElementById("message_container");
- elements.innerHTML+=` <div class="newUserjoin">
+  elements.innerHTML += ` <div class="newUserjoin">
  <span>${username}</span> Join the chat
 </div>`;
 });
 
 // send message to server so that all users receive it
 document.getElementById("forms").addEventListener("submit", (e) => {
-  var audio = new Audio('send.mp3');
+  var audio = new Audio("send.mp3");
   audio.play();
   e.preventDefault();
 
-setTimeout(()=>{
-  var elem = document.getElementById("message_container");
-  elem.scrollTop = elem.scrollHeight;
-},10);
-
-
+  setTimeout(() => {
+    var elem = document.getElementById("message_container");
+    elem.scrollTop = elem.scrollHeight;
+  }, 10);
 
   let messageInput = document.getElementById("msg").value;
   socket.emit("send-message", messageInput);
-// sound
-
+  // sound
 
   var elements = document.getElementById("message_container");
   let time = new Date().toLocaleTimeString([], {
@@ -87,7 +85,7 @@ setTimeout(()=>{
 
 // message receive which is send by any other connected user
 socket.on("receive-message", (data) => {
-  var audio = new Audio('receive.mp3');
+  var audio = new Audio("receive.mp3");
   audio.play();
   window.setInterval(function () {
     var elem = document.getElementById("message_container");
@@ -126,14 +124,12 @@ socket.on("receive-message", (data) => {
 // user disconnect
 
 socket.on("user-disconnect", (data) => {
-  var audio = new Audio('userleft.mp3');
+  var audio = new Audio("userleft.mp3");
   audio.play();
   const { username } = data;
   var elements = document.getElementById("message_container");
-elements.innerHTML+=`
+  elements.innerHTML += `
 <div class="newUserleft">
           <span>${username}</span> Left the chat
         </div>`;
 });
-
-
